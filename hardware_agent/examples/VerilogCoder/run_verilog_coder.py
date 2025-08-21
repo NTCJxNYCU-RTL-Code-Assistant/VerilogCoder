d0f752163ae72a3704af6aed826a49318971b128
Andy0857 <a265589@yahoo.com.tw>
2025-08-21 22:49:31 +0800

    add linting flow

--- hardware_agent/examples/VerilogCoder/run_verilog_coder.py
+++ hardware_agent/examples/VerilogCoder/run_verilog_coder.py
@@ -74,9 +74,9 @@ case_manager = VerilogCaseManager(file_path=args.verilog_example_dir,
 gpt4_config_list = config_list_from_json(env_or_file=args.oai_config)
 
 if gpt4_config_list[0]["model"] != "o3":
-    gpt4_config_list[0]["max_tokens"] = args.max_tokens
+    gpt4_config_list[0]["max_tokens"] = int(args.max_tokens)
 else:
-    gpt4_config_list[0]["max_completion_tokens"] = args.max_tokens
+    gpt4_config_list[0]["max_completion_tokens"] = int(args.max_tokens)
     gpt4_config_list[0]["temperature"] = 1
 
 # llama3 settings: Used for comparison
